1d869cac58e01c6ec5d3fd29fdc15ed6f35b649f
Jose <mariana.lemus.g15@gmail.com>
2022-04-24 00:28:50 +0200

    sync page

---
+++ src/components/elements/circle-icon.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import '../word.scss';
+import '../styles/sync.scss';
+import '../styles/myStyle.scss';
+import logo from '../../images/logo3.png';
+
+class IconCircle extends React.Component {
+
+    state = {
+    };
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div className={this.props.container} >
+                <div className='circle '>
+                    <div className='circle_title'>
+                        <h2>{this.props.h2}</h2>
+                        <h3>{this.props.h3}</h3>
+                    </div>
+                    <div className='circle_inner'>
+                        <div className='circle_inner__layer'>
+                            <img src={this.props.img} alt="..." class={this.props.imgClass}></img>
+                        </div>
+                        <div className='circle_inner__layer'>
+                            <img src={this.props.img} alt="..." class={this.props.imgClass}></img>
+                        </div>
+                        <div className='circle_inner__layer'>
+                            <img src={this.props.img} alt="..." class={this.props.imgClass}></img>
+                        </div>
+                    </div>
+                    <div className='content_shadow'></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default connect()(IconCircle);
--- src/components/pages/sync.js
+++ src/components/pages/sync.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import '../word.scss';
 import '../styles/sync.scss';
 import '../styles/myStyle.scss';
 import logo from '../../images/logo3.png';
+import Circle from '../elements/circle-icon';
 
 class Sync extends React.Component {
 
@@ -15,7 +15,6 @@ class Sync extends React.Component {
     }
 
 
-
     render() {
 
         return (
@@ -23,22 +22,25 @@ class Sync extends React.Component {
                 <div className="container heigh-sync">
                     <div className="row padding-center ">
                         <div className="col-4">
+
+                            <img src={logo} alt="..." class="icon-bg"></img>
                             <h1 className="h1-intro">Conectate</h1>
                             <h3 className='h3-intro'>Con tus herramientas de gestion</h3>
                         </div>
-                        <div className="col-2">
-                            <div className="row padding-center ">
+                        <div className="col-8  icons-sync">
+                            <div className="row ">
                                 <div className="col">
-                                    <img src="https://cdn.iconscout.com/icon/free/png-256/asana-226537.png" alt="..." class="rounded-circle icono-sync_bussi"></img>
+                                    <Circle container="container icon-top icon-circle" h2="Asana" h3="Sincroniza tu tablero" img="https://cdn.iconscout.com/icon/free/png-256/asana-226537.png" imgClass="icon-a" ></Circle>
                                 </div>
                                 <div className="col">
                                     <img src={logo} alt="..." class="icono-sync"></img>
                                 </div>
                                 <div className="col">
-                                    <img src="" alt="..." class="rounded-circle"></img>
+                                    <Circle container="container icon-left icon-circle" h2="Jira" h3="Sincroniza tu tablero" img="https://cdn-icons-png.flaticon.com/512/5968/5968875.png" imgClass="icon-jira" ></Circle>
+
                                 </div>
                                 <div className="col">
-                                    <img src="" alt="..." class="rounded-circle"></img>
+                                    <Circle container="container icon-right icon-circle" h2="Trello" h3="Sincroniza tu tablero" img="https://icons-for-free.com/download-icon-logo+social+trello+icon-1320194696754621808_512.png" imgClass="icon-trello" ></Circle>
                                 </div>
                             </div>
                         </div>
